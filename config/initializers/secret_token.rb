dd152c05bed4ffee0d16b02f195cd0a03b882141
Robby Clements <rclements@isotope11.com>
2010-09-02 12:43:20 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Zombie::Application.config.secret_token = '23993b317ef81d63c2d703bba6a3273bb15094db10df7e6ae8f69ce7cbe493656423fe88f49ca9e3947694e343814afa6aaa85516bf71c8b7a7e630e2384067c'
